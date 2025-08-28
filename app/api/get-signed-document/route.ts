b2e1ea1f1d4a52084c0f6e72fb9eb65522a97130
Echefula5 <eche@nimbussp.com>
2025-08-28 06:12:04 -0600

    fixed?

--- app/api/get-signed-document/route.ts
+++ app/api/get-signed-document/route.ts
@@ -3,11 +3,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextRequest, NextResponse } from "next/server";
 
 const s3Client = new S3Client({
-  region: process.env.HW_AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.HW_AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.HW_AWS_SECRET_ACCESS_KEY!,
-  },
+  region: process.env.HW_AWS_REGION!,
 });
 
 interface UploadRequestBody {
@@ -32,7 +28,8 @@ export async function POST(req: NextRequest) {
 
   const bucketName =
     process.env.HW_AWS_S3_HBX_BUCKET_NAME || "hbx-widget-datascource-da";
-
+  console.log(s3Client);
+  const test = process.env.HW_AWS_ACCESS_KEY_ID;
   try {
     const command = new GetObjectCommand({
       Bucket: bucketName,
@@ -45,6 +42,6 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ success: true, url: signedUrl });
   } catch (error: any) {
     console.error("Error getting signed URL:", error);
-    return NextResponse.json({ success: false, error: error.message });
+    return NextResponse.json({ success: false, error: error.message, test });
   }
 }
